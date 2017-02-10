06bee6fc704726c8c7c7107ff6c395d1e0d4c28d
Dynatrace-Jeroen-Hautekeete <jeroen.hautekeete@dynatrace.com>
2017-02-10 07:53:09 +0100

    Add mail-out properties for events
    Added Eclipse project support files.
    Add properties for mailing out individual events.
    Report #events as measure over evaluated time period
    Monitor task status is OK - independent of #events

---
+++ src/com/eventLogMonitor/EventMonitorActions.java
@@ -0,0 +1,51 @@
+package com.eventLogMonitor;
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+// import javax.activation.*;
+
+public class EventMonitorActions {
+	
+	
+	
+	
+	// File Name SendEmail.java
+
+
+	   public static void sendMail(String from,String to, String host, String subject,String text) {    
+	      // Get system properties
+	      Properties properties = System.getProperties();
+
+	      // Setup mail server
+	      properties.setProperty("mail.smtp.host", host);
+
+	      // Get the default Session object.
+	      Session session = Session.getDefaultInstance(properties);
+
+	      try{
+	         // Create a default MimeMessage object.
+	         MimeMessage message = new MimeMessage(session);
+
+	         // Set From: header field of the header.
+	         message.setFrom(new InternetAddress(from));
+
+	         // Set To: header field of the header.
+	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+	         // Set Subject: header field
+	         message.setSubject(subject);
+
+	         // Now set the actual message
+	         message.setText(text);
+
+	         // Send message
+	         Transport.send(message);
+	         System.out.println("Sent message successfully....");
+	      }catch (MessagingException mex) {
+	         mex.printStackTrace();
+	      }
+	   }
+	}	
+	
+	
